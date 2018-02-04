14ec4209ba18fffa44c5655f43c89c41c2ee8198
Leonard Law <leonwlaw@gmail.com>
2018-02-04 00:07:34 -0500

    Rudimentary redirection check

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from configparser import ConfigParser
 
 
 class CheckFailed(Exception):
@@ -6,16 +7,14 @@ class CheckFailed(Exception):
 
 
 def main():
-    checks = [
-        {
-            'uri': 'http://leonaard.me/',
-            'redirect_to': 'https://linkleonard.me/',
-        },
-    ]
-    for check in checks:
-        uri = check['uri']
+    config = ConfigParser()
+    config.read('config.ini')
+
+    for site_name in config.sections():
+        site = config[site_name]
+        uri = site['uri']
         response = requests.get(uri)
-        if response.url != check['redirect_to']:
+        if response.url != site['redirect_uri']:
             raise CheckFailed(uri)
 
 
