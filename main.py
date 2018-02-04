01823b16037e1096fdf10e36a0173fb613e84bfa
Leonard Law <leonwlaw@gmail.com>
2018-02-04 01:32:55 -0500

    Refactor code from main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from collections import OrderedDict
 from configparser import ConfigParser
 from argparse import ArgumentParser
 from requests.exceptions import RequestException
@@ -36,21 +37,14 @@ def get_argument_parser():
     return parser
 
 
-def main():
-    argument_parser = get_argument_parser()
-    options = argument_parser.parse_args()
-
-    config = ConfigParser()
-    config.read(options.config)
-
+def run_checks(check_config):
     failed_sites = []
 
-    for site_name in config.sections():
-        site = config[site_name]
-        check_class = getattr(checks, site['check'])
-        checker = check_class(site)
+    for site_name, site_config in check_config.items():
+        check_class = getattr(checks, site_config['check'])
+        checker = check_class(site_config)
 
-        uri = site['uri']
+        uri = site_config['uri']
         try:
             response = requests.get(uri)
         except RequestException as e:
@@ -64,24 +58,43 @@ def main():
             logger.error(f'Site check "{site_name}" failed: {repr(e)}')
             failed_sites.append([site_name, e])
 
+    return failed_sites
+
+
+def send_failed_notification(failed_sites, app_config):
+    email_body = format_errors_as_email_body(failed_sites)
+
+    recipients = app_config['notification-recipients']
+
+    mailgun_domain = app_config['mailgun-api-domain']
+    message_endpoint = f'https://api.mailgun.net/v3/{mailgun_domain}/messages'
+
+    requests.post(
+        message_endpoint,
+        auth=("api", app_config['mailgun-api-key']),
+        data={
+            "from": "Server Check <noreply@leonaard.me>",
+            "to": recipients,
+            "subject": "Server Check Failures",
+            "text": email_body
+        })
+
+
+def main():
+    argument_parser = get_argument_parser()
+    options = argument_parser.parse_args()
+
+    config = ConfigParser()
+    config.read(options.config)
+
+    failed_sites = run_checks(OrderedDict(
+        (site_name, config[site_name])
+        for site_name in config.sections()
+    ))
+
     if failed_sites and options.email_on_error:
-        email_body = format_errors_as_email_body(failed_sites)
-
         app_config = config['DEFAULT']
-        recipients = app_config['notification-recipients']
-
-        mailgun_domain = app_config['mailgun-api-domain']
-        message_endpoint = f'https://api.mailgun.net/v3/{mailgun_domain}/messages'  # noqa: E501
-
-        requests.post(
-            message_endpoint,
-            auth=("api", app_config['mailgun-api-key']),
-            data={
-                "from": "Server Check <noreply@leonaard.me>",
-                "to": recipients,
-                "subject": "Server Check Failures",
-                "text": email_body
-            })
+        send_failed_notification(failed_sites, app_config)
 
 
 if __name__ == '__main__':
